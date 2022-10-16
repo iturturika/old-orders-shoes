7617016d9efbd62f57106f9b95d6ab6455f25acd
iturturika <iturturika89@gmail.com>
2022-10-16 19:41:22 +0300

    small fixes

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -4,7 +4,7 @@ export const Banner = () => {
     return (
         <section className="banner">
         <div className="rightSection">
-          <h1>Old Orders, Get Now!</h1>
+          <h1><b style={{color: "#9ccfec"}}>Old Orders,</b> Get Now!</h1>
           <button className="button">Buy</button>
           </div>
           <div className="leftSection">
